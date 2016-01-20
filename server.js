4c5ccb1295ad834ffc63b51ac4ee82e194c05cd4
BABU <BABU@BABU-PC>
2016-01-21 00:21:34 +0530

    File upload

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 var express = require('express'),
-    
- kuponDeal = require('./routes/kuponDeal');
+	multer  = require('multer'),    
+	kuponDeal = require('./routes/kuponDeal');
 
 var app = express();
+var done=false;
 
+app.use(multer({ dest: 'C:/tmp/',
+    rename: function (fieldname, filename) {
+        return filename+"_"+Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...')
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+        done=true;
+    }
+}));
 
 app.use(function(req, res, next) {
 	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Credentials","true");
 	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	  res.header("Access-Control-Request-Headers","accept, content-type");
 	  res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE");
 	  next();
-});
-
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
 });
 
 
@@ -27,6 +36,17 @@ app.post('/saveKuponDeals', kuponDeal.addKuponDeal);
 app.put('/updateKuponDeal/:id', kuponDeal.updateKuponDeal);
 app.delete('/deleteKuponDeal/:id', kuponDeal.deleteKuponDeal);
 app.delete('/removeAllKuponDeal', kuponDeal.removeAllKuponDeal);
+
+app.post('/uploadDealImages',function(req,res){
+    if(done==true){
+        console.log(req.files);
+        res.end(JSON.stringify(req.files));
+    }
+});
+
+app.get('/uploadIndx',function(req,res){
+    res.sendfile("index.html");
+});
 
 app.listen(3000);
 console.log('Listening on port 3000...');
